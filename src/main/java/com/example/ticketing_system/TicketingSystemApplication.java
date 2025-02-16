da61ceeaeda042b7e1935830894793cf029c65cb
VishalAvs <satyavishal2000@gmail.com>
2025-02-16 17:27:30 -0500

    updated application properties to avoid cred leakage

--- src/main/java/com/example/ticketing_system/TicketingSystemApplication.java
+++ src/main/java/com/example/ticketing_system/TicketingSystemApplication.java
@@ -1,5 +1,6 @@
 package com.example.ticketing_system;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TicketingSystemApplication {
 
 	public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("MONGO_URI", dotenv.get("MONGO_URI"));
+		System.setProperty("MONGO_DB", dotenv.get("MONGO_DB"));
+		System.setProperty("COGNITO_USER_POOL_ID", dotenv.get("COGNITO_USER_POOL_ID"));
+		System.setProperty("COGNITO_CLIENT_SECRET", dotenv.get("COGNITO_CLIENT_SECRET"));
+		System.setProperty("COGNITO_CLIENT_ID", dotenv.get("COGNITO_CLIENT_ID"));
+
 		SpringApplication.run(TicketingSystemApplication.class, args);
 	}
 

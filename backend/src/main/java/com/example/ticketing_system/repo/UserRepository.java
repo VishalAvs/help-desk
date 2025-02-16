489b638260eb967434efb3865a58d479532f8dd2
VishalAvs <satyavishal2000@gmail.com>
2025-02-15 20:43:50 -0500

    Added user repo

---
+++ backend/src/main/java/com/example/ticketing_system/repo/UserRepository.java
@@ -0,0 +1,11 @@
+package com.example.helpdesk.repository;
+
+import com.example.helpdesk.model.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+    Optional<User> findByEmail(String email);
+}

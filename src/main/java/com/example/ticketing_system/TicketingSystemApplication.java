a712da4f93f8ce6c6fafa16bdb41d546770b475a
VishalAvs <satyavishal2000@gmail.com>
2025-03-06 16:13:38 -0500

    refactor

--- src/main/java/com/example/ticketing_system/TicketingSystemApplication.java
+++ src/main/java/com/example/ticketing_system/TicketingSystemApplication.java
@@ -10,14 +10,6 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
 public class TicketingSystemApplication {
 
 	public static void main(String[] args) {
-
-		Dotenv dotenv = Dotenv.load();
-		System.setProperty("MONGO_URI", dotenv.get("MONGO_URI"));
-		System.setProperty("MONGO_DB", dotenv.get("MONGO_DB"));
-		System.setProperty("COGNITO_USER_POOL_ID", dotenv.get("COGNITO_USER_POOL_ID"));
-		System.setProperty("COGNITO_CLIENT_SECRET", dotenv.get("COGNITO_CLIENT_SECRET"));
-		System.setProperty("COGNITO_CLIENT_ID", dotenv.get("COGNITO_CLIENT_ID"));
-
 		SpringApplication.run(TicketingSystemApplication.class, args);
 	}
 
